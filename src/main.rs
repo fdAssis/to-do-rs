441e3f3f3f3922b8f5ae1f25089b10c43fedf860
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-02 23:01:40 -0300

    refactor: create a to do item with to_do_factory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod to_do;
+use to_do::structs::traits::create::Create;
+use to_do::structs::{done::Done, pending::Pending};
+use to_do::to_do_factory;
+use to_do::ItemType;
+
 fn main() {
-    println!("Hello, world!");
+    let to_do_item = to_do_factory("pending", "washing");
+
+    match to_do_item.unwrap() {
+        ItemType::Peding(item) => item.create(&item.super_struct.title),
+        ItemType::Done(item) => println!(
+            "it's a done item with the title: {}",
+            item.super_struct.title
+        ),
+    }
 }
