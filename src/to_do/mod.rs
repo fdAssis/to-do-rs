973d115d5375a9e6eb802b2b7e28e48eca6ba49d
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-02 21:09:13 -0300

    feat: managing structs with factories

--- src/to_do/mod.rs
+++ src/to_do/mod.rs
@@ -1,1 +1,15 @@
 pub mod structs;
+use structs::{done::Done, pending::Pending};
+
+pub enum ItemType {
+    Peding(Pending),
+    Done(Done),
+}
+
+pub fn to_do_factory(item_type: &str, title: &str) -> Result<ItemType, &'static str> {
+    match item_type {
+        "pending" => Ok(ItemType::Peding(Pending::new(title))),
+        "done" => Ok(ItemType::Peding(Pending::new(title))),
+        _ => Err("This is not accepted"),
+    }
+}
