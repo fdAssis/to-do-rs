8288200515acdf7e59315fa3b63bcf76e53fa6be
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-11 16:56:44 -0300

    feat: add field last_update

--- src/to_do/structs/traits/edit.rs
+++ src/to_do/structs/traits/edit.rs
@@ -1,3 +1,4 @@
+use chrono::Local;
 use serde_json::value::Value;
 use serde_json::{json, Map};
 
@@ -5,14 +6,26 @@ use crate::state::write_to_file;
 use crate::FILE;
 pub trait Edit {
     fn set_to_done(&self, title: &String, state: &mut Map<String, Value>) {
-        state.insert(title.to_string(), json!(String::from("done")));
+        state.insert(
+            title.to_string(),
+            json!({
+            "last_update": format!("{}", Local::now().format("%e/%b/%Y-%H:%M:%S")),
+             "status":String::from("done")
+            }),
+        );
 
         write_to_file(FILE, state);
         println!("\n\n{} is being set to done\n\n", title);
     }
 
     fn set_to_pending(&self, title: &String, state: &mut Map<String, Value>) {
-        state.insert(title.to_string(), json!(String::from("pending")));
+        state.insert(
+            title.to_string(),
+            json!({
+            "last_update": format!("{}", Local::now().format("%e/%b/%Y-%H:%M:%S")),
+             "status":String::from("pending")
+            }),
+        );
         write_to_file(FILE, state);
         println!("\n\n{} is being set to pending\n\n", title);
     }
