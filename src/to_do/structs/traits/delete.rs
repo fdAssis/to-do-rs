7096ca335d0a18367bafd6b678a19665b44d8cf2
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-10 23:19:46 -0300

    refactor: delete entry into the state, and then use write_to_file function to write the updated state to the JSON file

--- src/to_do/structs/traits/delete.rs
+++ src/to_do/structs/traits/delete.rs
@@ -1,5 +1,14 @@
+use serde_json::value::Value;
+use serde_json::Map;
+
+use crate::state::write_to_file;
+use crate::FILE;
+
 pub trait Delete {
-    fn delete(&self, title: &str) {
-        println!("{} is being delete", title);
+    fn delete(&self, title: &String, state: &mut Map<String, Value>) {
+        state.remove(title);
+
+        write_to_file(FILE, state);
+        println!("\n\n{} is being deleted\n\n", title);
     }
 }
