39c997aa48c22815d9250011fa027922daafc67b
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-10 23:18:08 -0300

    refactor:  insert new entry into the state, and then use write_to_file function to write the updated state to the JSON file

--- src/to_do/structs/traits/create.rs
+++ src/to_do/structs/traits/create.rs
@@ -1,5 +1,14 @@
+use serde_json::value::Value;
+use serde_json::{json, Map};
+
+use crate::state::write_to_file;
+use crate::FILE;
+
 pub trait Create {
-    fn create(&self, title: &str) {
-        println!("{} is being create", title);
+    fn create(&self, title: &String, status: &String, state: &mut Map<String, Value>) {
+        state.insert(title.to_string(), json!(status));
+
+        write_to_file(FILE, state);
+        println!("\n\n{} is being create\n\n", title);
     }
 }
